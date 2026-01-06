4d729f0d0ad17231828f2f79c38b650630747f1e
Failfer <dceaton9@gmail.com>
2026-01-05 22:07:53 -0800

    added ultrasonic sensor part disabled the servo part
    disabled the servo part while im working on the ultrasonic sensor, I cant tell exactly how accurate it is due to my setup but it sounds roughly right (I only have 2 mtf wires and I need 4 so I'm using a pretty sketchy setup to make the voltage flow where it needs to go (ryhmes so its true)

--- Servo_practice_project.ino
+++ Servo_practice_project.ino
@@ -1,8 +1,16 @@
 #include <Servo.h>
+  const int triggerPin = 2;
+  const int echoPin = 7;
+
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(triggerPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+
+/*
   Servo servo;
-  servo.attach(12);
+  servo.attach(12); //also assigns the pin but using the built in servo.h 
   servo.write(0);
 
   delay(500);
@@ -28,10 +36,44 @@ void setup() {
   
   delay(500);
 
+*/
 
 }
+
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  long duration, cm;
+
+  digitalWrite(triggerPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(triggerPin, HIGH);
+  delayMicroseconds(10); //check datasheet for what num you need
+  digitalWrite(triggerPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+
+  cm = microsecondsToCentimeters(duration);
+
+
+
+  Serial.print("Distance: ");
+  Serial.print(cm);
+  Serial.println(" cm");
+
+
+  
+
+
+
 
 }
+
+long microsecondsToCentimeters(long ms){
+
+return ms / 29 /2; // divide by 2 since sound waves have to go to and from the object it hits to return 
+
+}
+
+
